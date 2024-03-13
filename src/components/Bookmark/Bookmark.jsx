461d29427cd148887fbb729965c89e07803d6b53
abunaim5 <abunaim140@gmail.com>
2024-03-14 02:38:46 +0600

    display the bookmarked blogs dynamically by handle click

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,9 +1,19 @@
-const Bookmark = ({bookmark}) => {
+import PropTypes from 'prop-types';
+
+const Bookmark = ({ bookmarks }) => {
     return (
-        <div>
-            <h3 className="font-semibold text-lg">Master Microsoft Power Platform and Become an In-Demand!</h3>
+        <div className='space-y-4'>
+            {
+                bookmarks.map((bookmark, idx) => <div key={idx} className="bg-white rounded-lg p-4">
+                    <h3 className="font-semibold text-lg">{bookmark.title}</h3>
+                </div>)
+            }
         </div>
     );
 };
+
+Bookmark.propTypes = {
+    bookmarks: PropTypes.array.isRequired
+}
 
 export default Bookmark;
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,18 +1,16 @@
 import Bookmark from "../Bookmark/Bookmark";
 import PropTypes from 'prop-types';
 
-const Bookmarks = ({bookmarks}) => {
+const Bookmarks = ({ bookmarks }) => {
     return (
         <div className="bg-gray-100 rounded-lg py-6 px-6 space-y-4">
             <h2 className="text-2xl font-bold">Bookmarked Blogs: {bookmarks.length}</h2>
-            <div className="bg-white rounded-lg p-4">
-                <Bookmark></Bookmark>
-            </div>
+            <Bookmark bookmarks={bookmarks}></Bookmark>
         </div>
     );
 };
 
-Bookmarks.propTypes ={
+Bookmarks.propTypes = {
     bookmarks: PropTypes.array.isRequired
 }
 
